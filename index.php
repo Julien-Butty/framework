05548739bc6875667fab4102a01d824ab8088555
Julien-Butty <julienbutty@gmail.com>
2017-04-21 09:58:31 +0200

    install composer

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
 
 // framework/index.php
-$input = $_GET['name'];
+$input =isset( $_GET['name']) ? $_GET['name'] :'World';
 
-printf('Hello %s', $input);
+header('Content-Type: text/html; charset=utf-8');
 
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES,
+    'ÚTF-8'
+    ));
 
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testHello()
+    {
+        $_GET['name'] = 'Fabien';
+
+        ob_start();
+        include 'index.php';
+        $content = ob_get_clean();
+
+        $this->assertEquals('Hello Fabien', $content);
+    }
+}
 ?>
